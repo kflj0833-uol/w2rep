8230cc00cc578630d6801e34e7affdb08ca386b7
kflj0833-uol <kflj0833@leeds.ac.uk>
2025-02-06 16:44:43 +0000

    new code

---
+++ search_list.py
@@ -0,0 +1,14 @@
+search_ordered_list(x,y):
+    sorted_lst=ordered_list(x)
+    low=0
+    high=len(sorted_lst)-1
+    while low<=high:
+        mid=(low+high)//2
+        if sorted_lst(mid)=y:
+            return True
+        elif sorted_lst(mid)<y:
+            low=mid+1
+        else:
+            high=mid-1
+    return false
+
