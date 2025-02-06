54d840523e13004aad1d956e5f9314855699d704
jbsp3416-uol <jbsp3416@leeds.ac.uk>
2025-02-06 16:56:48 +0000

    ammended version

--- search_list.py
+++ search_list.py
@@ -1,14 +1,16 @@
-search_ordered_list(x,y):
+from ordered_list import get_ordered_list
+
+def search_ordered_list(x,y):
     sorted_lst=get_ordered_list(x)
     low=0
     high=len(sorted_lst)-1
     while low<=high:
         mid=(low+high)//2
-        if sorted_lst(mid)=y:
+        if sorted_lst(mid)==y:
             return True
         elif sorted_lst(mid)<y:
             low=mid+1
         else:
             high=mid-1
-    return false
+    return False
 
